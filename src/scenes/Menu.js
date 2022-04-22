5cdc22289ef59441f1b0f6eb034ecf478138ea27
zachpaw1 <zac.2019@gmail.com>
2022-04-22 15:53:05 -0700

    Added in Control Scene, did a very rough version of the menu scene
    Also tried to get the enemy to reset, but wasn't able to

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,10 +1,54 @@
+// class Menu extends Phaser.Scene {
+//     constructor() {
+//         super("menuScene");
+//     }
+
+//     create() {
+//         this.add.text(20, 20, "Menu Scene");
+//         this.scene.start("playScene");
+//     }
+// }
 class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
 
     create() {
-        this.add.text(20, 20, "Menu Scene");
-        this.scene.start("playScene");
+
+        // menu text configuration
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '20px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+    // show menu text
+    this.add.text(game.config.width/2, game.config.height/2 - borderUISize * 2 - borderPadding * 2, '(Name Unknown)', menuConfig).setOrigin(0.5);
+    this.add.text(game.config.width/2, game.config.height/2 - borderUISize * 1 - borderPadding * 1, 'Use the ← and → arrows to move', menuConfig).setOrigin(0.5);
+    menuConfig.backgroundColor = '#00FF00';
+    menuConfig.color = '#000';
+    this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← arrow to start the game', menuConfig).setOrigin(0.5);
+    this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding * 5, 'Press → arrow for controls', menuConfig).setOrigin(0.5);
+
+
+    // define keys
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start('playScene');
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            this.scene.start('controlScene');
+        }
     }
 }

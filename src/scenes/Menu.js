51c5ade8181018629876269cd005993814904804
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2022-04-30 21:11:38 -0700

    added menu screen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,13 +3,16 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
+    preload() {
+        this.load.image('menu', './assets/MenuScreen.png');
+    }
+
     create() {
         // menu text configuration
         let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '20px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            fontFamily: 'Chiller',
+            fontSize: '50px',
+            color: '#9e1717',
             align: 'right',
             padding: {
               top: 5,
@@ -17,21 +20,21 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
+        // background image
+        this.add.image(0,0, 'menu').setOrigin(0,0);
 
-    // show menu text
-    this.add.text(game.config.width/2, game.config.height/2 - borderUISize * 2 - borderPadding * 2, 'Endless Color Rush', menuConfig).setOrigin(0.5);
-    menuConfig.backgroundColor = '#00FF00';
-    menuConfig.color = '#000';
-    this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← arrow to start the game', menuConfig).setOrigin(0.5);
-    this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding * 5, 'Press → arrow for controls', menuConfig).setOrigin(0.5);
-    this.highScoreText = this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding * 9, 'High Score: ' + highScore, menuConfig).setOrigin(0.5);
-    this.add.text(game.config.width/2, game.config.height/2 + borderUISize*3 + borderPadding * 7, 'Press R to reset High Score', menuConfig).setOrigin(0.5);
+        // show menu text
+        //this.add.text(game.config.width/2, game.config.height/2 - borderUISize * 2 - borderPadding * 2, 'Endless Color Rush', menuConfig).setOrigin(0.5);
+        //this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← arrow to start the game', menuConfig).setOrigin(0.5);
+        //this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding * 5, 'Press → arrow for controls', menuConfig).setOrigin(0.5);
+        this.highScoreText = this.add.text(game.config.width/2 + borderUISize * 3, game.config.height/2 + borderUISize + borderPadding * 6.4, highScore, menuConfig).setOrigin(0.5);
+        //this.add.text(game.config.width/2, game.config.height/2 + borderUISize*3 + borderPadding * 7, 'Press R to reset High Score', menuConfig).setOrigin(0.5);
 
 
-    // define keys
-    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
     }
 
     update() {
@@ -43,7 +46,7 @@ class Menu extends Phaser.Scene {
         }
         if (Phaser.Input.Keyboard.JustDown(keyR)) {
             localStorage.setItem("highStorage", 0);
-            this.highScoreText.text = 'High Score: ' + localStorage.getItem("highStorage");
+            this.highScoreText.text = localStorage.getItem("highStorage");
         }
     }
 }
